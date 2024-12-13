8653434a6ce4f6560a85b9ec9ba753be557b9461
okeck1982 <97165003+okeck1982@users.noreply.github.com>
2024-12-13 10:00:32 +0100

    Update basic-setup.sh

--- debian/basic-setup.sh
+++ debian/basic-setup.sh
@@ -9,12 +9,12 @@ C_INV='\033[0;31mINVALID\033[0m'
 INTERFACE=`ip link | awk '$0 ~ /^[0-9].*$/ && $2 != "lo:" { gsub(":","",$2); print $2 }'`
 
 # Get informations
-read -p "Admin Username (administrator): " ADMINUSER
-read -p "IP Address: " NEW_IP
-read -p "Netmaks (255.255.255.0): " NEW_MASK
-read -p "Gateway: " NEW_GW
-read -p "DNS Server (10.10.100.11): " NEW_DNS
-read -p "New Domain (ok.home): " NEW_DOMAIN
+read -p "Admin Username (administrator): " ADMINUSER < /dev/tty
+read -p "IP Address: " NEW_IP < /dev/tty
+read -p "Netmaks (255.255.255.0): " NEW_MASK < /dev/tty
+read -p "Gateway: " NEW_GW < /dev/tty
+read -p "DNS Server (10.10.100.11): " NEW_DNS < /dev/tty
+read -p "New Domain (ok.home): " NEW_DOMAIN < /dev/tty
 
 # Apply default Values
 ADMINUSER=${ADMINUSER:-administrator}
@@ -24,14 +24,14 @@ NEW_DOMAIN=${NEW_DOMAIN:-ok.home}
 
 # Pre Checks
 ip_regex='^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-echo -n "- CHECK: User '${ADMINUSER}' exists ... "; if [ `grep -e "^$ADMINUSER:.*$" /etc/passwd | wc -l` -eq 0 ]; then echo -e $C_ERR; exit 1; else echo -e $C_OK; fi
+echo -n "- CHECK: User '$ADMINUSER' exists ... "; if [ `grep -e "^${ADMINUSER}:.*$" /etc/passwd | wc -l` -eq 0 ]; then echo -e $C_ERR; exit 1; else echo -e $C_OK; fi
 echo -n "- CHECK: IP Address '${NEW_IP}' ... "; if [ `echo -n "${NEW_IP}" | grep -E $ip_regex | wc -l` -gt 0 ]; then echo -e $C_OK; else echo -e $C_INV; exit 1; fi
 echo -n "- CHECK: Netmask    '${NEW_MASK}' ... "; if [ `echo -n "${NEW_MASK}" | grep -E $ip_regex | wc -l` -gt 0 ]; then echo -e $C_OK; else echo -e $C_INV; exit 1; fi
 echo -n "- CHECK: Gateway IP '${NEW_GW}' ... "; if [ `echo -n "${NEW_GW}" | grep -E $ip_regex | wc -l` -gt 0 ]; then echo -e $C_OK; else echo -e $C_INV; exit 1; fi
 echo -n "- CHECK: DNS Server '${NEW_DNS}' ... "; if [ `echo -n "${NEW_DNS}" | grep -E $ip_regex | wc -l` -gt 0 ]; then echo -e $C_OK; else echo -e $C_INV; exit 1; fi
 
 # Overview
-cat << EOF
+cat << 'EOF'
 ===================================================================
 Setup Details:
  - Admin user is:     ${ADMINUSER}
@@ -55,7 +55,8 @@ ATTENTION: SYSTEM WILL AUTOMATICALY REBOOT
 ===================================================================
 
 EOF
-read -p "Execute changes? (y/n)" CONFIRM
+
+read -p "Execute changes? (y/n)" CONFIRM < /dev/tty
 if [ $CONFIRM != "y" ]; then exit 0; fi
 
 # Begin Setup
@@ -76,7 +77,7 @@ echo "$ADMINUSER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$ADMINUSER
 if [ $? -gt 0 ]; then echo -e $C_ERR; else echo -e $C_OK; fi
 
 echo -n "- Replace /etc/network/interfaces ..."
-cat << EOF > /home/administrator/interfaces
+cat << 'EOF' > /etc/network/interfaces
 # Loopback network interface
 auto lo
 iface lo inet loopback
@@ -90,6 +91,7 @@ iface ${INTERFACE} inet static
         dns-nameservers ${NEW_DNS}
         dns-domain ${NEW_DOMAIN}
 EOF
+
 if [ $? -gt 0 ]; then echo -e $C_ERR; else echo -e $C_OK; fi
 
 echo -n "- Disable IPv6 "
